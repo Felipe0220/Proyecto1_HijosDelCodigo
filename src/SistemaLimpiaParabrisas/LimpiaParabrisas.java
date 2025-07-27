fdca9e3480d8182c3616a95f88be07570c038b83
DavidSalazar <jdrsalazar1025@gmail.com>
2025-07-27 14:12:00 -0600

    clase limpiaparabrisas
    clase incompleta, faltan procesos, me embote

---
+++ src/SistemaLimpiaParabrisas/LimpiaParabrisas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SistemaLimpiaParabrisas;
+
+import Funcionalidades.Encendido;
+
+/**
+ *
+ * @author UTN
+ */
+public class LimpiaParabrisas extends Encendido {
+    //true=activado,false=desactivado
+    private boolean Delantero;
+    private boolean Trasero;
+    
+    private void activacion(){
+        
+    }
+    
+}
