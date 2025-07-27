1eb029a49370f720a0a091174aa80754de53986e
DavidSalazar <jdrsalazar1025@gmail.com>
2025-07-27 14:13:06 -0600

    clase velocidad
    clase de tipo enum, en ella se encuentra las velocidades de los limpia parabrisas, abierta  posibles cambios

---
+++ src/SistemaLimpiaParabrisas/Velocidad.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package SistemaLimpiaParabrisas;
+
+/**
+ *
+ * @author UTN
+ */
+public enum Velocidad {
+    RAPIDO,
+    MEDIO,
+    LENTO;
+    
+    private int velocidad;
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    private Velocidad() {
+        this.velocidad = velocidad;
+    } 
+    
+}
