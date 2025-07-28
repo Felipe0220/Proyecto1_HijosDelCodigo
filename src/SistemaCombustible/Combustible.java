5e73c36e17553a2c3793fdac0003b338a5321fc5
Kevinq <kevinquiros.m@gmail.com>
2025-07-28 11:06:22 -0600

    Clase Combustible
    Actualizaciones

--- src/SistemaCombustible/Combustible.java
+++ src/SistemaCombustible/Combustible.java
@@ -4,10 +4,39 @@
  */
 package SistemaCombustible;
 
+import Funcionalidades.Encendido;
+
 /**
  *
  * @author kevin
  */
-public class Combustible {
+public class Combustible extends Encendido {
+    private double nivel; 
     
+    public Combustible() {
+        this.nivel = 100; 
+    }
+    
+  
+    public void usarCombustible(double cantidad) {
+        if (!estado) {
+            System.out.println("¡El sistema está apagado!");
+            return;
+        }
+        nivel -= cantidad;
+        if (nivel < 0) nivel = 0;
+        System.out.println("Nivel de combustible: " + nivel + "%");
+    }
+    
+   
+    public void rellenar(double cantidad) {
+        nivel += cantidad;
+        if (nivel > 100) nivel = 100;
+        System.out.println("¡Combustible rellenado! Nivel: " + nivel + "%");
+    }
+    
+    
+    public double getNivel() {
+        return nivel;
+    }
 }
